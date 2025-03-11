520e75f59ce33bfb3d29b216f143f82ac0518f3e
ravijaanthony <ravijaanthony@gmail.com>
2025-03-11 19:19:38 +0530

    refactor: update file upload

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,14 @@ import "./App.css";
 
 function App() {
   const [messages, setMessages] = useState<string[]>([]);
+  const nodeServerUrl  = import.meta.env.NODE_SERVER_URL;
 
   // Function to send the dropped/selected file to the backend
   const sendToBackend = async (file: File) => {
     const formData = new FormData();
     formData.append("file", file);
     try {
-      await axios.post("http://localhost:5000/upload", formData, {
+      await axios.post(`http://143.110.249.212/upload`, formData, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
@@ -26,7 +27,7 @@ function App() {
       console.error("Error sending file:", error);
       setMessages((prevMessages) => [
         ...prevMessages,
-        `Error uploading ${file.name}: ${error}`
+        `Error uploading ${file.name}: ${error}, ${nodeServerUrl}`
       ]);
     }
   };
